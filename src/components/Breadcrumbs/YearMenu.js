9338ee626f4bb8f3898e227b78f82b14bddc8638
Ivan <30-02@mail.ru>
2025-12-22 14:01:06 +0500

    2026

--- src/components/Breadcrumbs/YearMenu.js
+++ src/components/Breadcrumbs/YearMenu.js
@@ -12,11 +12,12 @@ export default function YearMenu() {
     const years = useMemo(
         () => (
             [
-                {value: 2022, label: '2022'},
-                {value: 202212, label: '2022/12'},
-                {value: 2023, label: '2023'},
-                {value: 2024, label: '2024'},
+                {value: 2026, label: '2026'},
                 {value: 2025, label: '2025'},
+                {value: 2024, label: '2024'},
+                {value: 2023, label: '2023'},
+                {value: 202212, label: '2022/12'},
+                {value: 2022, label: '2022'},
             ]
         ),
         []
